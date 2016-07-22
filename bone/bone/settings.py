8cffce8823fd64955745983458dc6ffe2fff23e1
Patrick Insinger <patrickinsinger@gmail.com>
2016-07-22 12:36:20 -0400

    Added compressor

--- bone/bone/settings.py
+++ bone/bone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrapform',
     'sorl.thumbnail',
+    'compressor',
 
     'boneweb',
     'portalweb',
@@ -136,6 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+]
+
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
