da183b0ad5035ded3d828f7a817a8149ac77b843
Patrick Insinger <patrickinsinger@gmail.com>
2016-07-27 09:16:37 -0400

    Add bulk resident hide/show functionality to admin

--- bone/boneweb/admin.py
+++ bone/boneweb/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Resident
 
+def make_hidden(modeladmin, request, queryset):
+    queryset.update(visible=False)
+make_hidden.short_description = "Hide selected residents"
+
+def make_visible(modeladmin, request, queryset):
+    queryset.update(visible=True)
+make_visible.short_description = "Show selected residents"
+
 class ResidentAdmin(admin.ModelAdmin):
     list_display = ('name', 'kerberos', 'bio', 'visible')
+    actions = [make_hidden, make_visible]
 
 admin.site.register(Resident, ResidentAdmin)
