d7f10dbe797328b5ca3b28e2ab086678784d011d
Patrick Insinger <patrickinsinger@gmail.com>
2016-07-26 22:40:11 -0400

    Add shibboleth support

--- bone/portalweb/auth.py
+++ bone/portalweb/auth.py
@@ -1,24 +1,23 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
-import hmac
-import hashlib
 
 from django.shortcuts import render, redirect
 from boneweb.models import Resident
 
 from django.conf import settings
 from django.contrib.auth import login, logout, authenticate
-from django.utils.crypto import get_random_string
 
-class ScriptsAuthenticationBackend(ModelBackend):
-    def authenticate(self, token="", email="", signature=""):
-        if self._verify_token(token, email, signature):
+from urllib.parse import urlencode
+
+class ShibbolethAuthenticationBackend(ModelBackend):
+    def authenticate(self, email=""):
+        if self._verify_email(email):
             kerberos = self._extract_kerberos(email)
             try:
                 user = User.objects.get(username=kerberos)
             except User.DoesNotExist:
                 try:
-                    resident = Resident.objects.get(kerberos=kerberos)
+                    resident = Resident.objects.get(kerberos=kerberos) #ONLY MAKE IF USER EXISTS
                     user = User(username=kerberos)
                     user.save()
                     resident.user = user
@@ -29,12 +28,8 @@ class ScriptsAuthenticationBackend(ModelBackend):
         return None
 
     @staticmethod
-    def _verify_token(token, email, signature):
-        if not email.lower().endswith('@mit.edu'):
-            return False
-        message = "{}:{}".format(token, email).encode('utf-8')
-        h = hmac.new(settings.SCRIPTS_AUTH_KEY, message, hashlib.sha256)
-        return hmac.compare_digest(h.hexdigest(), signature)
+    def _verify_email(email):
+        return email.lower().endswith('@mit.edu')
 
     @staticmethod
     def _extract_kerberos(email):
@@ -43,23 +38,17 @@ class ScriptsAuthenticationBackend(ModelBackend):
 def login_view(request):
     if request.user.is_authenticated():
         return redirect('profile')
-    email = request.GET.get('email')
-    signature = request.GET.get('signature')
-    token = request.session.get('token')
-    if email and signature and token:
-        del request.session['token']
-        user = authenticate(token=token, email=email, signature=signature)
+    print(request.META)
+    email = request.META.get('HTTP_EPPN')
+    if email:
+        user = authenticate(email=email)
         if user:
             login(request, user)
             return redirect('profile')
         return render(request, 'portalweb/authentication_failed.html')
-    elif request.GET.get('failed'):
-        if token:
-            del request.session['token']
-        return render(request, 'portalweb/authentication_failed.html')
-    token = get_random_string(length=32)
-    request.session['token'] = token
-    return redirect('{}?token={}'.format(settings.SCRIPTS_AUTH_URL, token))
+    target_uri = request.build_absolute_uri(request.path)
+    redirect_qs = urlencode({'target': target_uri})
+    return redirect("{}?{}".format(settings.SHIB_LOGIN_URL, redirect_qs))
 
 def logout_view(request):
     if request.user.is_authenticated():
