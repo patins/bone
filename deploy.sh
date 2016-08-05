77cbb791c47d0a4836f660bd8d8a0b4c750083e1
Patrick Insinger <patrickinsinger@gmail.com>
2016-08-04 22:27:03 -0400

    Changed Order and added collectstatic to deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ cd ~/bone
 git pull
 docker-compose -f docker-compose.prod.yml build app
 docker-compose -f docker-compose.prod.yml up --no-deps -d app
+docker-compose -f docker-compose.prod.yml exec app python3 manage.py collectstatic --no-input
 exit
 EOF
