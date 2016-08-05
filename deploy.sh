30d07eb978811ac286980090c0d92df344c9a3ae
Patrick Insinger <patrickinsinger@gmail.com>
2016-08-04 23:02:10 -0400

    Remove static file deployment until further testing

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,5 @@ cd ~/bone
 git pull
 docker-compose -f docker-compose.prod.yml build app
 docker-compose -f docker-compose.prod.yml up --no-deps -d app
-docker-compose -f docker-compose.prod.yml exec app python3 manage.py collectstatic --no-input
 exit
 EOF
