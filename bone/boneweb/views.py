b1f5f612f77affe6c55db58ccbc3bf9a9b1b2bb3
Patrick Insinger <patins@users.noreply.github.com>
2016-08-04 23:20:37 -0400

    Switch alumni status to boolean DB flag instead of year logic (#21)

--- bone/boneweb/views.py
+++ bone/boneweb/views.py
@@ -15,36 +15,26 @@ def home(request):
 def about(request):
     return render(request, 'boneweb/about.html')
 
-def _current_graduation_year(t=timezone.now()):
-    """ Returns the year that graduates should be considering given a datetime"""
-    if t.month > 4: # If it's after May it's this year
-        return t.year
-    else: # If it's before June it's last year
-        return t.year - 1
-
 def residents(request):
-    grad_year = _current_graduation_year()
-    visible_residents = Resident.objects.filter(visible=True, year__gt=grad_year)
+    visible_residents = Resident.objects.filter(visible=True, alumni=False)
     ordered_residents = visible_residents.order_by('year', 'name')
     all_years = visible_residents.values_list('year', flat=True).distinct().order_by('year')
-    visible_alums = Resident.objects.filter(visible=True, year__lte=grad_year)
+    visible_alums = Resident.objects.filter(visible=True, alumni=True)
     ordered_alums = visible_alums.order_by('year', 'name')
     return render(request, 'boneweb/residents.html', {'residents': ordered_residents, 'alumni': ordered_alums, 'all_years': all_years})
 
 def residents_by_year(request, year):
-    grad_year = _current_graduation_year()
-    all_years = Resident.objects.filter(visible=True, year__gt=grad_year).values_list('year', flat=True).distinct().order_by('year')
+    all_years = Resident.objects.filter(visible=True, alumni=False).values_list('year', flat=True).distinct().order_by('year')
     visible_residents = Resident.objects.filter(visible=True, year=year).order_by('name')
     if visible_residents.count() == 0:
         raise Http404()
-    visible_alums = Resident.objects.filter(visible=True, year__lte=grad_year)
+    visible_alums = Resident.objects.filter(visible=True, alumni=True)
     return render(request, 'boneweb/residents.html', {'residents': visible_residents, 'year': year, 'all_years': all_years, 'alumni': visible_alums})
 
 def alumni(request):
-    grad_year = _current_graduation_year()
-    visible_residents = Resident.objects.filter(visible=True, year__gt=grad_year)
+    visible_residents = Resident.objects.filter(visible=True, alumni=False)
     all_years = visible_residents.values_list('year', flat=True).distinct().order_by('year')
-    visible_alums = Resident.objects.filter(visible=True, year__lte=grad_year)
+    visible_alums = Resident.objects.filter(visible=True, alumni=True)
     ordered_alums = visible_alums.order_by('year', 'name')
     return render(request, 'boneweb/residents.html', {'residents': ordered_alums, 'alumni': ordered_alums, 'all_years': all_years})
 
