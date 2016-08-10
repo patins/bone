58d8b1b59eab777a2797f148af4cdf661ad7ba85
Patrick Insinger <patrickinsinger@gmail.com>
2016-08-09 23:26:39 -0400

    Added Tinder

--- bone/portalweb/admin.py
+++ bone/portalweb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tinder
 
-# Register your models here.
+class TinderAdmin(admin.ModelAdmin):
+    list_display = ('resident', 'name', 'age', 'location')
+    search_fields = ('name', 'location', 'bio')
+
+admin.site.register(Tinder, TinderAdmin)
--- bone/portalweb/views.py
+++ bone/portalweb/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from boneweb.models import Resident
-from .forms import ResidentForm
+from .models import Tinder
+from .forms import ResidentForm, TinderForm
 
 @login_required
 def profile(request):
@@ -15,3 +16,20 @@ def profile(request):
     else:
         form = ResidentForm(instance=resident)
         return render(request, 'portalweb/profile.html', { 'resident': resident, 'form': form })
+
+@login_required
+def tinder(request):
+    resident = request.user.resident
+    try:
+        tinder = resident.tinder
+    except Tinder.DoesNotExist:
+        tinder = Tinder.objects.create(resident=resident, name=resident.name, picture=resident.picture)
+    if request.method == 'POST':
+        form = TinderForm(request.POST, request.FILES, instance=tinder)
+        if form.is_valid():
+            form.save()
+            form = TinderForm(instance=tinder)
+        return render(request, 'portalweb/tinder.html', { 'resident': resident, 'form': form })
+    else:
+        form = TinderForm(instance=tinder)
+        return render(request, 'portalweb/tinder.html', { 'resident': resident, 'form': form })
