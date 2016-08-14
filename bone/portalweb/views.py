d9f85fd4b43d322c3b213caa8fd0b85afcdcc48c
Patrick Insinger <patrickinsinger@gmail.com>
2016-08-13 23:39:57 -0400

    Fix tinder print view

--- bone/portalweb/views.py
+++ bone/portalweb/views.py
@@ -73,6 +73,6 @@ def tinders(request):
 @login_required
 @permission_required('tinders.view_all', raise_exception=True)
 def tinders_print(request):
-    residents = Resident.objects.filter(Q(alumni=False) | Q(tinder__isnull=False)).order_by('alumni', 'year', 'name')
+    residents = Resident.objects.filter(tinder__isnull=False).order_by('alumni', 'year', 'name')
     residents = residents.exclude(TINDER_NOT_COMPLETED_SPECS)
     return render(request, 'portalweb/tinders_print.html', { 'residents': residents })
