735adde4df942d947c4d2f227d29137f6b7436e1
Patrick Insinger <patins@users.noreply.github.com>
2016-08-13 23:34:51 -0400

    Added Tinder management features (#38)

--- bone/portalweb/views.py
+++ bone/portalweb/views.py
@@ -35,8 +35,44 @@ def tinder(request):
         form = TinderForm(instance=tinder)
         return render(request, 'portalweb/tinder.html', { 'resident': resident, 'form': form })
 
+TINDER_NOT_COMPLETED_SPECS = Q(tinder__bio="") | Q(tinder__age="") |\
+                             Q(tinder__name="") | Q(tinder__picture="") |\
+                             Q(tinder__location="")
+
+def tinders_metrics():
+    residents_not_alumni = Resident.objects.filter(alumni=False)
+    total = residents_not_alumni.count()
+    if total == 0:
+        return None
+    not_started = residents_not_alumni.filter(tinder__isnull=True).count()
+    completed = residents_not_alumni.exclude(tinder__isnull=True).exclude(TINDER_NOT_COMPLETED_SPECS).count()
+    metrics = {
+        'not_started': not_started,
+        'not_started_pct': 100.0 * not_started / total,
+        'completed': completed,
+        'completed_pct': 100.0 * completed / total
+    }
+    metrics['started'] = total - metrics['not_started'] - metrics['completed']
+    metrics['started_pct'] = 100.0 - metrics['not_started_pct'] - metrics['completed_pct']
+    return metrics
+
 @login_required
 @permission_required('tinders.view_all', raise_exception=True)
 def tinders(request):
+    only_not_completed = request.GET.get('not_completed') != None
     residents = Resident.objects.filter(Q(alumni=False) | Q(tinder__isnull=False)).order_by('alumni', 'year', 'name')
-    return render(request, 'portalweb/tinders.html', { 'residents': residents })
+    if only_not_completed:
+        residents = residents.filter(TINDER_NOT_COMPLETED_SPECS)
+    residents = residents.prefetch_related('tinder', 'user')
+    return render(request, 'portalweb/tinders.html', {
+        'residents': residents,
+        'only_not_completed': only_not_completed,
+        'metrics': tinders_metrics()
+    })
+
+@login_required
+@permission_required('tinders.view_all', raise_exception=True)
+def tinders_print(request):
+    residents = Resident.objects.filter(Q(alumni=False) | Q(tinder__isnull=False)).order_by('alumni', 'year', 'name')
+    residents = residents.exclude(TINDER_NOT_COMPLETED_SPECS)
+    return render(request, 'portalweb/tinders_print.html', { 'residents': residents })
