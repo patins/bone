935bfad7f755018e4e75f04ba366f425fcceb927
Patrick Insinger <patins@users.noreply.github.com>
2016-08-31 13:01:54 -0400

    Remove Tinder (#43)
    * Removed Tinder
    
    * Added print styles

--- bone/portalweb/forms.py
+++ bone/portalweb/forms.py
@@ -1,13 +1,8 @@
 from django.forms import ModelForm
 from boneweb.models import Resident
-from .models import Tinder
 
 class ResidentForm(ModelForm):
     class Meta:
         model = Resident
         fields = ['name', 'picture', 'bio', 'visible']
 
-class TinderForm(ModelForm):
-    class Meta:
-        model = Tinder
-        fields = ['name', 'age', 'location', 'bio', 'picture']
--- bone/portalweb/urls.py
+++ bone/portalweb/urls.py
@@ -18,9 +18,6 @@ from . import views, auth
 
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^tinder/$', views.tinder, name='tinder'),
-    url(r'^tinders/$', views.tinders, name='tinders'),
-    url(r'^tinders/print/$', views.tinders_print, name='tinders_print'),
     url(r'^login/$', auth.login_view, name='login'),
     url(r'^logout/$', auth.logout_view, name='logout')
 ]
